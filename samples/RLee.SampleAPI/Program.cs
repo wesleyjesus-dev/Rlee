d926cc7d8dc93321c5d77c9b88e01870f942e5d3
wesleyjesus-dev <76056225+wesleyjesus-dev@users.noreply.github.com>
2024-03-17 00:18:25 -0300

    add project sample

---
+++ samples/RLee.SampleAPI/Program.cs
@@ -0,0 +1,21 @@
+﻿using RLee.Core.Frontend;
+using RLee.Core.Frontend.Material;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    var scaffold = new Scaffold()
+        .SetAppBar(new AppBar())
+        .SetBody(
+            new Column()
+            .SetChildren(new Row())
+        )
+        .SetBottomNavigationBar(new Row());
+
+    return Results.Ok(new Column().SetChildren(new Row()));
+});
+
+app.Run();
+
--- tests/RLee.UnitTests/UnitTest1.cs
+++ tests/RLee.UnitTests/UnitTest1.cs
@@ -1,46 +1,14 @@
-﻿namespace RLee.UnitTests;
+﻿using RLee.Core.Frontend;
+
+namespace RLee.UnitTests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
-    {
-
-    }
-
-    [Fact]
-    public void WhenCreateAColumnShoudHaveARow()
+    public void WhenAddRowInColumnTheColumnShouldBeAItem()
     {
         var column = new Column();
         column.AddChildren(new Row());
         Assert.Contains(new Row(), column.Childrens);
     }
-}
-
-public class Widget
-{
-    public Widget()
-    {
-        Key = Guid.NewGuid().ToString();
-    }
-
-    public Widget(string key) => Key = key;
-
-    public string Key { get; set; }
-}
-
-public class Column : Widget
-{
-    public List<Widget> Childrens { get; private set; } = new();
-
-    public Column AddChildren(Widget widget)
-    {
-        Childrens.Add(widget);
-        return this;
-    }
-}
-
-public class Row : Widget
-{
-    public List<Widget> Children { get; set; } = new();
 }
