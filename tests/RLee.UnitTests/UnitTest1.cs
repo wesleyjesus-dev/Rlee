ce8798746d07228101f7add00f89d1f9bc0766f5
wesleyjesus-dev <76056225+wesleyjesus-dev@users.noreply.github.com>
2024-03-16 22:07:27 -0300

    create project

---
+++ tests/RLee.UnitTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿namespace RLee.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void WhenCreateAColumnShoudHaveARow()
+    {
+        var column = new Column();
+        column.AddChildren(new Row());
+        Assert.Contains(new Row(), column.Childrens);
+    }
+}
+
+public class Widget
+{
+    public Widget()
+    {
+        Key = Guid.NewGuid().ToString();
+    }
+
+    public Widget(string key) => Key = key;
+
+    public string Key { get; set; }
+}
+
+public class Column : Widget
+{
+    public List<Widget> Childrens { get; private set; } = new();
+
+    public Column AddChildren(Widget widget)
+    {
+        Childrens.Add(widget);
+        return this;
+    }
+}
+
+public class Row : Widget
+{
+    public List<Widget> Children { get; set; } = new();
+}
